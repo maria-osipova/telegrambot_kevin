80e784fa83c4e5e97665b40ccbb5baafca211391
Maria Osipova <maryosipovaa@gmail.com>
2023-11-27 14:11:51 -0300

    added notifications to the owner, with information about the user's response

--- 1.py
+++ 1.py
@@ -1,15 +1,16 @@
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot('6774068511:AAFLW29l_cz1slADWNIp1u8C1WbYaFsRpwI')
+bot = telebot.TeleBot('6774068511:AAFLW29l_cz1slADWNIp1ud8C1WbYaFsRpwI')
 
 # Dictionary to store the user's messages
 user_responses = {}
 
+
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.InlineKeyboardMarkup()
-    btn1 = types.InlineKeyboardButton('да', callback_data='yes')
+    btn1 = types.InlineKeyboardButton('да', callback_data='yes'
     btn2 = types.InlineKeyboardButton('нет', callback_data='no')
     markup.row(btn1, btn2)
 
@@ -19,11 +20,15 @@ def start(message):
 def callback_message(callback):
     user_id = callback.from_user.id
     chat_id = callback.message.chat.id
+    user_name =  callback.from_user.first_name
+    choice = callback.data
 
     if callback.data == 'yes':
         user_responses[user_id] = 'окей! спасибо, что предупредил!'
+        bot.send_message('635793659', 'купи сендвичи')
     elif callback.data == 'no':
         user_responses[user_id] = 'окей, спасибо, что предупредил.'
+        bot.send_message('635793659', 'не покупай')
 
     # Edit the original message to include the user's response
     if user_id in user_responses:
