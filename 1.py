497c405e56aa81ab9f9b0f43a054fc4900f2410a
Maria Osipova <maryosipovaa@gmail.com>
2024-01-02 15:10:08 -0300

    added right schedule, added users, to whom messages are sent

--- 1.py
+++ 1.py
@@ -8,7 +8,6 @@ bot = telebot.TeleBot('6774068511:AAFLW29l_cz1slADWNIp1u8C1WbYaFsRpwI')
 
 user_responses = {}
 user_data = {}
-
 @bot.message_handler(commands=['start'])
 def start(message):
     user_id = message.chat.id
@@ -17,7 +16,7 @@ def start(message):
     user_data[user_id] = {'chat_id': user_id, 'user_name': user_name}
     print("Received /start from user:", user_id, "User data:", user_data)
 
-    bot.send_message(user_id, 'Привет!')
+    bot.send_message(user_id, 'Привет! Я бот, который поможет тебе не забывать про сендвичи для Кевина. Я отправляю напоминания ')
 
 def schedule_checker():
     while True:
@@ -38,31 +37,34 @@ def callback_message(callback):
         bot.send_message('635793659', 'купи сендвичи')
     elif callback.data == 'no':
         user_responses[user_id] = 'окей, спасибо, что предупредил.'
-        bot.send_message('635793659', 'не покупай')
+        bot.send_message('635793659', 'не покупай сендвичи')
 
     if user_id in user_responses:
         bot.edit_message_text(user_responses[user_id], chat_id, callback.message.message_id)
-        bot.delete_message(chat_id, callback.message.message_id - 1)
+
 def function_to_run():
-    if not user_data:  # Проверка, что в user_data есть данные
-        print("No messages received yet.")
-        return
+    specific_users = [358408249, 6149317579]  # Список chat_id конкретных пользователей
 
-    for user_id, details in user_data.items():
-        chat_id = details['chat_id']
-        user_name = details['user_name']
-        markup = types.InlineKeyboardMarkup()
-        btn1 = types.InlineKeyboardButton('да', callback_data='yes')
-        btn2 = types.InlineKeyboardButton('нет', callback_data='no')
-        markup.add(btn1, btn2)
+    for user_id in specific_users:
+        if user_id in user_data:  # Проверяем, есть ли пользователь в user_data
+            chat_id = user_data[user_id]['chat_id']
+            user_name = user_data[user_id]['user_name']
+            markup = types.InlineKeyboardMarkup()
+            btn1 = types.InlineKeyboardButton('да', callback_data='yes')
+            btn2 = types.InlineKeyboardButton('нет', callback_data='no')
+            markup.add(btn1, btn2)
 
-        try:
-            bot.send_message(chat_id, f'Привет, {user_name}! Придет ли сегодня Кевин?', reply_markup=markup)
-        except Exception as e:
-            print(f"Error sending message to {chat_id}: {e}")  # Логирование ошибки
+            try:
+                bot.send_message(chat_id, f'Привет, {user_name}! Придёт ли сегодня Кевин?', reply_markup=markup)
+            except Exception as e:
+                print(f"Error sending message to {chat_id}: {e}")  # Логирование ошибки
+        else:
+            print(f"User {user_id} not found in user_data")
 
 if __name__ == "__main__":
-    schedule.every().tuesday.at("14:25").do(function_to_run)
+    schedule.every().tuesday.at("08:00").do(function_to_run)
+    schedule.every().tuesday.at("08:00").do(function_to_run)
+    schedule.every().friday.at("08:00").do(function_to_run)
     Thread(target=schedule_checker).start()
     bot.polling(non_stop=True)
 
